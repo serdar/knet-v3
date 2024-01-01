9b06951c6a1eafbf7a1b46a83c45b41a14b299e3
Serdar Kilic <serdar@kilic.net>
2024-01-01 21:56:27 +1100

    wip

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,12 +2,17 @@ import { Hono } from "hono";
 import { cors } from 'hono/cors'
 
 import { getPosts } from './database/app'
+import { serve } from "@hono/node-server";
 
 const app = new Hono();
 app.use('/', cors())
 app.get('/', async (c) => { 
    const posts = await getPosts();
    return c.json(posts)
+});
+
+serve(app, (opt) => {
+   console.log(`Hono is on port: ${opt.port}`)
 });
 
 export default app;
