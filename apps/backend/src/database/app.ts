fe68b1b4dcd4153b63817182527fdc4524e70efd
Serdar Kilic <serdar@kilic.net>
2024-01-02 22:44:59 +1100

    updated db calls

--- apps/backend/src/database/app.ts
+++ apps/backend/src/database/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import { drizzle } from 'drizzle-orm/planetscale-serverless'
 import { connect } from '@planetscale/database'
 import * as schema from '../database/schema/post'
+import { eq } from 'drizzle-orm'
 
 // create the connection
 export const connection = connect({
@@ -10,14 +11,18 @@ export const connection = connect({
 
 export const db = drizzle(connection, {schema});
 
-export async function getPosts() {
-  //db stuff
-  console.log(`db`);
-  return await db.query.posts.findFirst();
+export async function getPost(id: number) {
+  return await db.query.posts.findMany({
+    where: eq(schema.posts.id, id)
+  })
 }
 
-export async function getPostBySlug() {
-  //db stuff
-  console.log(`db`);
-  return await db.query.posts.findFirst();
+export async function getPosts() {
+  return await db.query.posts.findMany();
+}
+
+export async function getPostBySlug(slug: string) {
+  return await db.query.posts.findMany({
+    where: eq(schema.posts.slug, slug)
+  });
 }
