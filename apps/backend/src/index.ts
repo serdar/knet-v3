8c11a6e28ebcbc049f21cc5ad927674e34e6e1d0
Serdar Kilic <serdar@kilic.net>
2024-01-03 14:45:09 +1100

    removed bun as it's still experimental under wsl

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cors } from 'hono/cors'
 
-import { getPosts } from './database/app'
+import { getPosts, getPostBySlug } from './database/app'
 import { serve } from "@hono/node-server";
 
 const app = new Hono();
@@ -9,6 +9,14 @@ const app = new Hono();
 app.use('/', cors())
 app.get('/', async (c) => { 
    const posts = await getPosts();
+   return c.json(posts)
+});
+
+// Get by Slug
+app.get('/:slug', async (c) => { 
+   const slug = c.req.param('slug')
+   const posts = await getPostBySlug(slug);
+
    return c.json(posts)
 });
 
